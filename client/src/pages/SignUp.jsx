2555293eeea91fc4b9e2d7e13acffafa80f607a1
COllins <collinsmuiruri77@gmail.com>
2023-11-01 17:05:58 +0300

    Completed signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,30 +1,69 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function SignUp() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await axios.post("/api/auth/signup", {
+        username: userName,
+        email,
+        password,
+      });
+      setLoading(false);
+      navigate("/sign-in");
+      console.log(res.data);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+    setUserName("");
+    setEmail("");
+    setPassword("");
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-semibold text-3xl my-7 text-center">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="p-3 border rounded-lg"
+          value={userName}
           id="username"
+          onChange={(e) => setUserName(e.target.value)}
         />
         <input
           type="email"
           placeholder="email"
           className="p-3 border rounded-lg"
+          value={email}
           id="email"
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="password"
           className="p-3 border rounded-lg"
+          value={password}
           id="password"
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="text-white bg-slate-900 p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign up
+        <button
+          disabled={loading}
+          className="text-white bg-slate-900 p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -33,6 +72,7 @@ function SignUp() {
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500">{error}</p>}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/users", userRouter);
-app.use("/api/users", authRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
