93cbabb2518b8f322475f9351a79aea43190aae1
COllins <collinsmuiruri77@gmail.com>
2023-11-02 20:56:27 +0300

    Added redux-persist into the store.

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import {
   usersSliceReducer,
   signInStart,
@@ -6,14 +8,28 @@ import {
   signInFailure,
 } from "./users/usersSlice";
 
+const rootReducer = combineReducers({
+  users: usersSliceReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    users: usersSliceReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware();
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
   },
   devTools: true,
 });
 
 export { store, signInStart, signInSuccess, signInFailure };
+
+export const persistor = persistStore(store);
--- client/src/store/users/usersSlice.js
+++ client/src/store/users/usersSlice.js
@@ -1,5 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// const testInitialState = {
+//   currentUser: localStorage.getItem("userInfo")
+//     ? JSON.parse(localStorage.getItem("userInfo"))
+//     : null,
+//   error: null,
+//   loading: false,
+// };
 const initialState = {
   currentUser: null,
   error: null,
@@ -16,6 +23,7 @@ const usersSlice = createSlice({
     signInSuccess: (state, action) => {
       state.loading = false;
       state.currentUser = action.payload;
+      //   localStorage.setItem("userInfo", JSON.stringify(action.payload));
     },
     signInFailure: (state, action) => {
       state.loading = false;
