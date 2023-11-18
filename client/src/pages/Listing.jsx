aa1a429ad6ed34fd47483520a077bd1089d18d00
COllins <collinsmuiruri77@gmail.com>
2023-11-18 21:20:41 +0300

    Listing page display functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -5,6 +5,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import {
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaCarSide,
+} from "react-icons/fa";
 
 function Listing() {
   SwiperCore.use([Navigation]);
@@ -13,16 +19,16 @@ function Listing() {
     params.listingId
   );
 
-  const [listing, setListing] = useState(null);
-  useEffect(() => {
-    const getListing = async () => {
-      const res = await data;
+  // const [listing, setListing] = useState(null);
+  // useEffect(() => {
+  //   const getListing = async () => {
+  //     const res = await data;
 
-      setListing(res);
-      console.log(listing);
-    };
-    getListing();
-  }, []);
+  //     setListing(res);
+  //     console.log(listing);
+  //   };
+  //   getListing();
+  // }, [params.listingId]);
   return (
     <main>
       {isError && <div>Could Not Get Listing</div>}
@@ -43,7 +49,7 @@ function Listing() {
           </div>
         </div>
       )}
-      {listing && !isError && !isLoading && (
+      {data && !isError && !isLoading && (
         <div>
           <Swiper
             spaceBetween={30}
@@ -57,9 +63,9 @@ function Listing() {
             }}
             navigation={true}
             modules={[Autoplay, Pagination, Navigation]}
-            className="mySwiper"
+            className="h-[550px] mySwiper"
           >
-            {listing.imageUrls.map((url) => {
+            {data.imageUrls.map((url) => {
               return (
                 <SwiperSlide key={url}>
                   <div
@@ -73,6 +79,39 @@ function Listing() {
               );
             })}
           </Swiper>
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+            <p className="text-2xl font-semibold">
+              {data.name} - ${" "}
+              {data.offer
+                ? data.discountPrice.toLocaleString("en-US")
+                : data.regularPrice.toLocaleString("en-US")}
+              {data.type === "lease" && " / month"}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
+              <FaMapMarkerAlt className="text-green-700" />
+              {data.modelYear}
+            </p>
+            <div className="flex gap-4">
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                {data.type === "lease" ? "For Lease" : "For Sale"}
+              </p>
+              {data.offer && (
+                <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                  ${+data.regularPrice - +data.discountPrice} OFF
+                </p>
+              )}
+            </div>
+            <p className="text-slate-800">
+              <span className="font-semibold text-black">Description - </span>
+              {data.description}
+            </p>
+            <ul className="text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaCarSide className="text-lg" />
+                {data.units && `${data.units} available`}
+              </li>
+            </ul>
+          </div>
         </div>
       )}
     </main>
